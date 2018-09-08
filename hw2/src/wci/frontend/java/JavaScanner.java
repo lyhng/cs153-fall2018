bf4a7d19265ed3cb13acc92ab3e7302685999116
Justin Ocampo <lingling287@gmail.com>
2018-09-07 20:51:40 -0700

    added java comment functionality

--- hw2/src/wci/frontend/java/JavaScanner.java
+++ hw2/src/wci/frontend/java/JavaScanner.java
@@ -4,95 +4,107 @@ import wci.frontend.*;
 import wci.frontend.java.tokens.*;
 
 import static wci.frontend.Source.EOF;
+import static wci.frontend.Source.EOL;
 import static wci.frontend.java.JavaTokenType.*;
 import static wci.frontend.java.JavaErrorCode.*;
 
 /**
  * <h1>JavaScanner</h1>
  *
- * <p>The Java scanner.</p>
+ * <p>
+ * The Java scanner.
+ * </p>
  *
- * <p>Copyright (c) 2009 by Ronald Mak</p>
- * <p>For instructional purposes only.  No warranties.</p>
+ * <p>
+ * Copyright (c) 2009 by Ronald Mak
+ * </p>
+ * <p>
+ * For instructional purposes only. No warranties.
+ * </p>
  */
-public class JavaScanner extends Scanner
-{
-    /**
-     * Constructor
-     * @param source the source to be used with this scanner.
-     */
-    public JavaScanner(Source source)
-    {
-        super(source);
-    }
+public class JavaScanner extends Scanner {
+	/**
+	 * Constructor
+	 * 
+	 * @param source the source to be used with this scanner.
+	 */
+	public JavaScanner(Source source) {
+		super(source);
+	}
 
-    /**
-     * Extract and return the next Java token from the source.
-     * @return the next token.
-     * @throws Exception if an error occurred.
-     */
-    protected Token extractToken()
-        throws Exception
-    {
-        skipWhiteSpace();
+	/**
+	 * Extract and return the next Java token from the source.
+	 * 
+	 * @return the next token.
+	 * @throws Exception if an error occurred.
+	 */
+	protected Token extractToken() throws Exception {
+		skipWhiteSpace();
 
-        Token token;
-        char currentChar = currentChar();
+		Token token;
+		char currentChar = currentChar();
 
-        // Construct the next token.  The current character determines the
-        // token type.
-        if (currentChar == EOF) {
-            token = new EofToken(source);
-        }
-        else if (Character.isLetter(currentChar)) {
-            token = new JavaWordToken(source);
-        }
-        else if (Character.isDigit(currentChar)) {
-            token = new JavaNumberToken(source);
-        }
-        else if (currentChar == '\'') {
-            token = new JavaStringToken(source);
-        }
-        else if (JavaTokenType.SPECIAL_SYMBOLS
-                 .containsKey(Character.toString(currentChar))) {
-            token = new JavaSpecialSymbolToken(source);
-        }
-        else {
-            token = new JavaErrorToken(source, INVALID_CHARACTER,
-                                         Character.toString(currentChar));
-            nextChar();  // consume character
-        }
+		// Construct the next token. The current character determines the
+		// token type.
+		if (currentChar == EOF) {
+			token = new EofToken(source);
+		} else if (Character.isLetter(currentChar)) {
+			token = new JavaWordToken(source);
+		} else if (Character.isDigit(currentChar)) {
+			token = new JavaNumberToken(source);
+		} else if (currentChar == '\'') {
+			token = new JavaStringToken(source);
+		} else if (JavaTokenType.SPECIAL_SYMBOLS.containsKey(Character.toString(currentChar))) {
+			token = new JavaSpecialSymbolToken(source);
+		} else {
+			token = new JavaErrorToken(source, INVALID_CHARACTER, Character.toString(currentChar));
+			nextChar(); // consume character
+		}
 
-        return token;
-    }
+		return token;
+	}
 
-    /**
-     * Skip whitespace characters by consuming them.  A comment is whitespace.
-     * @throws Exception if an error occurred.
-     */
-    private void skipWhiteSpace()
-        throws Exception
-    {
-        char currentChar = currentChar();
+	/**
+	 * Skip whitespace characters by consuming them. A comment is whitespace.
+	 * 
+	 * @throws Exception if an error occurred.
+	 */
+	private void skipWhiteSpace() throws Exception {
+		char currentChar = currentChar();
 
-        while (Character.isWhitespace(currentChar) || (currentChar == '{')) {
+		while (Character.isWhitespace(currentChar) || (currentChar == '/')) {
+			// Start of a comment?
+			if (currentChar == '/') {
+				currentChar = nextChar();
 
-            // Start of a comment?
-            if (currentChar == '{') {
-                do {
-                    currentChar = nextChar();  // consume comment characters
-                } while ((currentChar != '}') && (currentChar != EOF));
+				// start of * comment
+				if (currentChar == '*') {					
+					while (true) {
+						currentChar = nextChar();
+						
+						// if found */ sequence break from loop
+						if (currentChar == '*' && source.peekChar() == '/') break;
+					}
 
-                // Found closing '}'?
-                if (currentChar == '}') {
-                    currentChar = nextChar();  // consume the '}'
-                }
-            }
+					// Found closing '*'?
+					if (currentChar == '*') {
+						currentChar = nextChar(); // consume the '*'
+						currentChar = nextChar(); // consume the '/'
+					}
+				}
 
-            // Not a comment.
-            else {
-                currentChar = nextChar();  // consume whitespace character
-            }
-        }
-    }
+				// start of double back slash comment
+				else if (currentChar == '/') {
+					do {
+						currentChar = nextChar(); // consume comment characters
+					} while (currentChar != EOL);
+				}
+
+			}
+			// Not a comment.
+			else {
+				currentChar = nextChar(); // consume whitespace character
+			}
+		}
+	}
 }
