c279ea3402c7b50c28530104df8084f9ddca07ef
Ly Nguyen <ly.nguyen@sslmda.com>
2018-12-05 07:19:42 -0800

    finished turtle graphics initially, need some tweaks and animation (optional)

--- hw6/code/cmm-runtime/src/cmm_runtime/CmmRuntime.java
+++ hw6/code/cmm-runtime/src/cmm_runtime/CmmRuntime.java
@@ -1,5 +1,8 @@
 package cmm_runtime;
 
+
+import java.awt.geom.Rectangle2D;
+
 public class CmmRuntime {
   public static void print(int x) {
     System.out.println(x);
@@ -25,4 +28,64 @@ public class CmmRuntime {
 
     System.out.printf("Total run time: %dms\n", duration);
   }
+
+  /* LOGO */
+
+  private static TurtleGraphics turtleGraphics = new TurtleGraphics();
+
+  static void start_logo() {
+    turtleGraphics.start();
+  }  // initialize the GUI canvas here. We can ask the user to run this before using any of the following commands
+
+
+  static void forward(int distance) {
+    turtleGraphics.move(distance);
+  }  // move the turtle by distance
+
+  static void right(int angle) {
+    turtleGraphics.rotate(-angle);
+  } // clockwise rotating
+
+  static void left(int angle) {
+    turtleGraphics.rotate(angle);
+  }  // counter-clockwise rotating
+
+  static void square(int length) {
+    turtleGraphics.drawSquare(length);
+  }  // draw a square with the current coordinate as the left bottom corner of the square
+
+  static void wait(int seconds) {
+    try {
+      Thread.sleep(seconds);
+    }
+    catch (InterruptedException e) {
+      // TODO: Handle this
+    }
+  }  // Thread.sleep
+
+  static void clearscreen() {
+    turtleGraphics.clear();
+    turtleGraphics.resetTurtle();
+  }  // reset, clear the screen and set turtle to origin.
+
+  static void penup() {
+    turtleGraphics.setPen(false);
+  }    // stop leaving trace on route, i.e. disable drawing only move the turtle
+
+  static void pendown() {
+    turtleGraphics.setPen(true);
+  }   // start leaving trace on route, i.e. enable drawing
+
+  static void hideturtle() {
+    turtleGraphics.setTurtleVisible(false);
+  }  // make the turtle invisible
+
+  static void showturtle() {
+    turtleGraphics.setTurtleVisible(true);
+  } // make the turtle visible
+
+
+  static void home() {
+    turtleGraphics.resetTurtle();
+  } // set turtle to origin, but not clear the screen
 }
---
+++ hw6/code/cmm-runtime/src/cmm_runtime/TurtleGraphics.java
@@ -0,0 +1,148 @@
+package cmm_runtime;
+
+import javafx.scene.Scene;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+
+public class TurtleGraphics extends JPanel {
+
+  /* Turtle class*/
+  private static class Turtle {
+
+    // Length for drawing arrow head
+    private static final double ARROW_LENGTH = 20.0f;
+
+    double x, y;
+    double angle;
+    boolean visible;
+    Color color;
+    boolean enable;
+
+    Turtle(double x, double y, double angle) {
+      this.x = x;
+      this.y = y;
+      this.angle = angle;
+      this.visible = true;
+      this.color = Color.BLACK;
+      this.enable = true;
+    }
+
+    // Compute paths for drawing arrow head
+    private Path2D calcDrawPath() {
+      // End point
+      double endX = this.x;
+      double endY = this.y;
+
+      double arrowWidth = ARROW_LENGTH * 0.5f;
+
+      Path2D.Float drawPath = new Path2D.Float();
+      drawPath.moveTo ( endX - ARROW_LENGTH, endY - arrowWidth );
+      drawPath.lineTo ( endX, endY );
+      drawPath.lineTo ( endX - ARROW_LENGTH, endY + arrowWidth );
+      drawPath.lineTo ( endX - ARROW_LENGTH * 0.6f, endY );
+      drawPath.lineTo ( endX - ARROW_LENGTH, endY - arrowWidth );
+
+      return drawPath;
+    }
+
+    void draw(Graphics2D g) {
+      g.fill ( calcDrawPath() );
+    }
+  }
+
+  private static final int CANVAS_WIDTH = 800;
+  private static final int CANVAS_HEIGHT = 800;
+
+  private Turtle turtle;
+  private ArrayList<Shape> scene;
+
+  public TurtleGraphics() {
+    this.scene = new ArrayList<>();
+    this.turtle = new Turtle(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, 0);
+  }
+
+  public void start() {
+    JFrame frame = new JFrame("Turtle Graphics");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    frame.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
+    frame.setLocationByPlatform(true);
+    this.setSize(frame.getSize());
+    this.setOpaque(true);
+    this.setBackground(Color.WHITE);
+    frame.getContentPane().add(BorderLayout.CENTER, this);
+    frame.setVisible(true);
+  }
+
+  public void setPen(boolean penStatus) {
+    turtle.enable = penStatus;
+  }
+
+  public void setTurtleVisible(boolean isVisible) {
+    this.turtle.visible = isVisible;
+  }
+
+  public void drawSquare(int length) {
+    Rectangle2D rec = new Rectangle2D.Double(turtle.x, turtle.y - length, length, length);
+    if (turtle.enable)
+      scene.add(rec);
+  }
+
+  // Assuming angle is in degrees
+  public void rotate(int angle) {
+    double rad = angle * (Math.PI / 180);
+    turtle.angle += rad;
+    turtle.angle %= 2 * Math.PI;
+  }
+
+  public void move(int distance) {
+    double dx = 1 * Math.cos(-turtle.angle);
+    double dy = 1 * Math.sin(-turtle.angle);
+    Line2D.Double line = new Line2D.Double();
+    line.x1 = turtle.x;
+    line.y1 = turtle.y;
+    line.x2 = line.x1 + dx * distance;
+    line.y2 = line.y1 + dy * distance;
+    turtle.x = line.x2;
+    turtle.y = line.y2;
+    if (turtle.enable)
+      scene.add(line);
+  }
+
+  public void clear() {
+    scene.clear();
+  }
+
+  public void resetTurtle() {
+    turtle.x = CANVAS_WIDTH / 2;
+    turtle.y = CANVAS_HEIGHT / 2;
+    turtle.angle = 0;
+  }
+
+  @Override
+  public void paintComponent(Graphics g) {
+    Graphics2D g2d = (Graphics2D)g;
+
+    g2d.setRenderingHint(
+            RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_ON);
+    g2d.setBackground(Color.WHITE);
+    g2d.setColor(turtle.color);
+
+    // Hack to draw arrow head:
+    // Rotate canvas -> draw turtle -> rotate back
+    g2d.rotate(-turtle.angle, turtle.x, turtle.y);
+    if (turtle.visible) turtle.draw(g2d);
+    g2d.rotate(turtle.angle, turtle.x, turtle.y);
+
+    // Draw scene
+    for (Shape s : scene) {
+      g2d.draw(s);
+    }
+  }
+}
